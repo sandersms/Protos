314bf604fb07b04ed42b1d66e9709337853ae75b
Mark Sanders <marksanders194@gmail.com>
2023-07-13 15:15:45 +0000

    Update go GRPC gateway for package structure
    Signed-off-by: Mark Sanders <marksanders194@gmail.com>

--- nwgate/cmd/main.go
+++ nwgate/cmd/main.go
@@ -9,7 +9,11 @@ import (
 	"log"
 	"net"
 
+	inv "github.com/opiproject/opi-api/common/v1/gen/go"
+	"github.com/sandersms/Protos/nwgate/pkg/inventory"
+
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 var (
@@ -25,6 +29,9 @@ func main() {
 		log.Fatalf("listen failed: %v", err)
 	}
 	grpcServer := grpc.NewServer()
+
+	inv.RegisterInventorySvcServices(grpcServer, &inventory.Server{})
+	reflection.Register(grpcServer)
 
 	log.Printf("Server listening at %v", lis.Addr())
 	if err := grpcServer.Serve(lis); err != nil {
