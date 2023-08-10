880fed0a16165db8030770f35ebf84ba00e975b4
Mark Sanders <marksanders194@gmail.com>
2023-08-10 11:37:42 -0400

    Add the dpu device file for the main server function
    Signed-off-by: Mark Sanders <marksanders194@gmail.com>

---
+++ nwgate/pkg/dpudev/dpudev.go
@@ -0,0 +1,24 @@
+// Copyright (c) xxxx, .
+
+// Package dpudev (DPU Device) is the main package of the application
+package dpudev
+
+import (
+	"fmt"
+
+	pb "github.com/opiproject/opi-api/common/v1/gen/go"
+)
+
+// The Server Object
+type Server struct {
+	pb.UnimplementedInventorySvcServer
+	Dpus map[string]*pb.Inventory
+}
+
+// Create initialized instance of the Dpu Device server
+func NewServer() *Server {
+	fmt.Println("Creating DPU Device Server")
+	return &Server{
+		Dpus: make(map[string]*pb.Inventory),
+	}
+}
