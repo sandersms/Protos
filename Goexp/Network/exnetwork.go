d0af2f2252203138b0605b75f2b8d69d7a37d791
Mark Sanders <marksanders194@gmail.com>
2023-08-28 21:12:00 +0000

    Changes to experiment network netlink operations to retrieve data and place in structure
    Signed-off-by: Mark Sanders <marksanders194@gmail.com>

--- Goexp/Network/exnetwork.go
+++ Goexp/Network/exnetwork.go
@@ -7,10 +7,10 @@ import (
 	// ipb "github.com/sandersms/Protos/protobuf/network/opencfg/v1alpha1/gen/go"
 )
 
-type Netintfs struct {
-	Intfs map[string]*Ifports
-	// Intfs map[string]*ipb.NetInterface
-}
+//type Netintfs struct {
+//	Intfs map[string]*Ifports
+// Intfs map[string]*ipb.NetInterface
+//}
 
 type Ifports struct {
 	name   string
@@ -25,23 +25,30 @@ func main() {
 	}
 
 	//Initialize the datastore
-	Intfs := make(map[string]*Ifports)
+	Intfs := make(map[int]*Ifports)
 	fmt.Println("Intfs:", Intfs)
 
 	// Output the parameters retrieved for the list of links
+	var idx int = 0
 	for _, intf := range net_intfs {
+		fmt.Println("type", intf.Type())
+		fmt.Println("Index", intf.Attrs().Index)
+		fmt.Println("Name", intf.Attrs().Name)
+		fmt.Println("Flags", intf.Attrs().Flags)
+		fmt.Println("MTU", intf.Attrs().MTU)
+		fmt.Println("Raw Attributes", intf.Attrs())
 		if intf.Type() == "device" {
 			netif := new(Ifports)
-			fmt.Println("type", intf.Type())
-			fmt.Println("Index", intf.Attrs().Index)
-			fmt.Println("Name", intf.Attrs().Name)
-			fmt.Println("Raw Attributes", intf.Attrs())
 			// Add the information to the Interface structure for the fixed interfaces
 			netif.name = intf.Attrs().Name
 			netif.iftype = intf.Type()
-			fmt.Println("NetInterface:", netif)
-			Intfs = netif
+			//fmt.Println("NetInterface:", netif)
+			Intfs[idx] = netif
+			// increment index
+			idx++
 		}
 	}
-	// fmt.Println(intfList)
+	for i := range Intfs {
+		fmt.Println(Intfs[i].name)
+	}
 }
