4833a4551bfdcf1654bc8a8e946729a51596861d
Mark Sanders <marksanders194@gmail.com>
2024-07-23 16:34:09 -0500

    feat(nwgate): correctly initialize physical interfaces in platform
    Signed-off-by: Mark Sanders <marksanders194@gmail.com>

--- nwgate/go.mod
+++ nwgate/go.mod
@@ -6,7 +6,7 @@ toolchain go1.22.0
 
 require (
 	github.com/jaypipes/ghw v0.12.0
-	github.com/opiproject/opi-api v0.0.0-20240722141955-49f6e67a6639
+	github.com/opiproject/opi-api v0.0.0-20240723152412-3a7e0d20e63b
 	github.com/vishvananda/netlink v1.1.0
 	go.einride.tech/aip v0.68.0
 	golang.org/x/sys v0.22.0
@@ -27,8 +27,8 @@ require (
 	github.com/vishvananda/netns v0.0.4 // indirect
 	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20240722135656-d784300faade // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240722135656-d784300faade // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240723171418-e6d459c13d2a // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240723171418-e6d459c13d2a // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	howett.net/plist v1.0.1 // indirect
 )
--- nwgate/pkg/dpudev/Interface.go
+++ nwgate/pkg/dpudev/Interface.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 
 	"github.com/sandersms/Protos/nwgate/pkg/utils"
 	"github.com/vishvananda/netlink"
@@ -14,18 +15,6 @@ import (
 
 	ipb "github.com/opiproject/opi-api/network/opinetcommon/v1alpha1/gen/go"
 )
-
-type Ifports struct {
-	name     string
-	ifindex  int
-	iftype   string
-	mtu      int
-	loopback bool
-	enabled  bool
-	st_admin bool
-	st_oper  string
-	logical  bool
-}
 
 // InitNetworkData initialize the network device information
 func InitNetworkData(s *Server) {
@@ -36,28 +25,63 @@ func InitNetworkData(s *Server) {
 	}
 
 	// Output the parameters retrieved for the list of links
-	var ifloop bool = false
 	for _, intf := range net_intfs {
+		var ifloop bool = false
+		var ifenabled bool = false
+		var ifadmin ipb.AdminState = ipb.AdminState_ADMIN_STATE_UNSPECIFIED
+		var ifoper ipb.OperState = ipb.OperState_OPER_STATE_UNSPECIFIED
+
 		fmt.Println("type", intf.Type())
 		fmt.Println("Index", intf.Attrs().Index)
 		fmt.Println("Name", intf.Attrs().Name)
-		fmt.Println("Flags", intf.Attrs().Flags)
+		// Process the interface flags to get loopback and Admin state
+		ifflags := intf.Attrs().Flags
+		fmt.Println("Flags", ifflags)
+		if ifflags&net.FlagUp != 0 {
+			ifenabled = true
+			ifadmin = ipb.AdminState_ADMIN_STATE_ENABLE
+		} else {
+			ifadmin = ipb.AdminState_ADMIN_STATE_DISABLE
+		}
+		if ifflags&net.FlagLoopback != 0 {
+			fmt.Println("Loopback on interface")
+			ifloop = true
+		}
 		fmt.Println("MTU", intf.Attrs().MTU)
-		fmt.Println("OperState", intf.Attrs().OperState)
+		// Check and map the operational state reported by the interface
+		ostate := intf.Attrs().OperState
+		fmt.Println("OperState", ostate)
+		switch ostate {
+		case netlink.OperUnknown:
+			ifoper = ipb.OperState_OPER_STATE_UNKNOWN
+		case netlink.OperNotPresent:
+			ifoper = ipb.OperState_OPER_STATE_NOT_PRESENT
+		case netlink.OperDown:
+			ifoper = ipb.OperState_OPER_STATE_DOWN
+		case netlink.OperLowerLayerDown:
+			ifoper = ipb.OperState_OPER_STATE_LOWER_LAYER_DOWN
+		case netlink.OperTesting:
+			ifoper = ipb.OperState_OPER_STATE_TESTING
+		case netlink.OperDormant:
+			ifoper = ipb.OperState_OPER_STATE_DORMANT
+		case netlink.OperUp:
+			ifoper = ipb.OperState_OPER_STATE_UP
+		default:
+			ifoper = ipb.OperState_OPER_STATE_UNSPECIFIED
+		}
 		fmt.Println("Raw Attributes", intf.Attrs())
 		if intf.Attrs().RawFlags&unix.IFF_LOOPBACK != 0 {
 			fmt.Println("Loopback interface")
-			ifloop = true
 		}
 
 		fmt.Println("----------")
 		// check for an actual device and ethernet interface
 		if intf.Type() == "device" && intf.Attrs().EncapType == "ether" {
 			// check if the device exists
-			netif, ok := s.Intfs[intf.Attrs().Name]
+			_, ok := s.Intfs[intf.Attrs().Name]
 			if ok {
 				log.Printf("Already existing interface %v", intf.Attrs().Name)
-				fmt.Println("Interface %v exists", netif)
+				continue
 			}
 			// Create the device in the list
 			fmt.Println("Create the physical device in the list")
@@ -68,12 +92,19 @@ func InitNetworkData(s *Server) {
 				Type:         ipb.InterfaceType_INTERFACE_TYPE_ETHERNET,
 				Mtu:          uint32(intf.Attrs().MTU),
 				LoopbackMode: ifloop,
+				Description:  "",
+				Enabled:      ifenabled,
 			}
 			Netintf.State = &ipb.NetInterfaceState{
 				Name:         intf.Attrs().Name,
 				Type:         ipb.InterfaceType_INTERFACE_TYPE_ETHERNET,
 				Mtu:          uint32(intf.Attrs().MTU),
 				LoopbackMode: ifloop,
+				Description:  "",
+				Enabled:      ifenabled,
+				Ifindex:      uint32(intf.Attrs().Index),
+				AdminState:   ifadmin,
+				OperState:    ifoper,
 			}
 
 			// add the interface to the detected set by adding to the list.
@@ -82,28 +113,10 @@ func InitNetworkData(s *Server) {
 				Config: Netintf.Config,
 				State:  Netintf.State,
 			}
-
-			//			netif := new(s.Intfs)
-			// Add the information to the Interface structure for the fixed interfaces
-			//			netif.name = intf.Attrs().Name
-			//			netif.ifindex = intf.Attrs().Index
-			//			netif.iftype = intf.Attrs().EncapType
-			//			netif.mtu = intf.Attrs().MTU
-			//			if intf.Attrs().RawFlags&unix.IFF_LOOPBACK != 0 {
-			//				netif.loopback = true
-			//			}
-			//			netif.st_oper = fmt.Sprint(intf.Attrs().OperState)
-			//			if intf.Attrs().RawFlags&unix.IFF_UP != 0 {
-			//				netif.st_admin = true
-			//			}
-			//fmt.Println("NetInterface:", netif)
-			//			Intfs[idx] = netif
-			// increment index
-			//			idx++
 		}
 	}
 	for _, Intfs := range s.Intfs {
-		fmt.Println(Intfs.Name, Intfs.State.Name, Intfs.State.Type, Intfs.State.Mtu, Intfs.State.LoopbackMode)
+		fmt.Println(Intfs.Name, Intfs.State.Type, Intfs.State.Mtu, Intfs.State.LoopbackMode, Intfs.State.Enabled, Intfs.State.Ifindex, Intfs.State.AdminState, Intfs.State.OperState)
 	}
 }
 
